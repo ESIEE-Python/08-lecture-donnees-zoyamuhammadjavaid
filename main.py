136b15a767d156b833e8d2cd8a57cf03fa61194e
zoyamuhammadjavaid <zoya.muhammadjavaid@edu.esiee.fr>
2024-10-07 12:52:05 +0000

    Voici le programme de l'exercice sur la lecture d données sur les fichiers

--- main.py
+++ main.py
@@ -14,27 +14,46 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
+    with open(filename, mode = 'r', encoding = 'utf-8') as file:
+        lines = file.readlines()
+        l = [[int(i) for i in line.split(';')] for line in lines]
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+     if k < 0 or k >= len(data):
+        raise IndexError("Indice hors limite".format(len(data)-1))
+     l = data[k]
+     return l
 
 def get_first(l):
-    return None
+    if len(l) == 0:
+        return None
+    else:
+        return l[0]
 
 def get_last(l):
-    return None
+    if len(l) == 0:
+        return None
+    else:
+        return l[-1]
 
 def get_max(l):
-    return None
+    if len(l) == 0:
+        return None
+    else:
+        return max(l)
 
 def get_min(l):
-    return None
+    if len(l) == 0:
+        return None
+    else:
+        return min(l)
 
 def get_sum(l):
-    return None
+    if len(l) == 0:
+        return None
+    else:
+        return sum(l)
 
 
 #### Fonction principale
@@ -47,6 +66,26 @@ def main():
     #     print(i, l)
     # k = 37
     # print(k, get_list_k(data, 37))
+    try:
+        data = read_data(FILENAME)
+        print("Données qui seront lues depuis le fichier :", data)
+
+        if data:
+            print("Liste à l'indice 0:", get_list_k(data, 0))
+            print("Premier élément de la première liste :", get_first(data[0]))
+            print("Dernier élément de la première liste :", get_last(data[0]))
+            print("Maximum de la première liste :", get_max(data[0]))
+            print("Minimum de la première liste :", get_min(data[0]))
+            print("Somme :", get_sum(data[0]))
+        else:
+            print("Le fichier est vide")
+
+    except FileNotFoundError:
+        print(f"Erreur : le fichier '{FILENAME}' n'a pas été trouvé.")
+    except IndexError as e:
+        print(e)
+
+
 
 
 if __name__ == "__main__":
